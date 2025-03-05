89c723c72780f23636826eadd0aaa028195c06a5
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-03-05 18:36:53 -0500

    first commit

---
+++ componets/ButtonLogin.js
@@ -0,0 +1,16 @@
+ 
+ import Link from "next/link";
+
+const ButtonLogin = ({ isLoggedIn, name }) => {
+  if (isLoggedIn) {
+    return (
+      <Link href="/dashboard" className="btn btn-primary">
+        Welcome back, {name}
+      </Link>
+    );
+  }
+
+  return <button className="btn btn-primary">Login</button>;
+};
+
+export default ButtonLogin;
