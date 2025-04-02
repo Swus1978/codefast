1e8e70d38b5d8ec73a46ca70ca69e9011444a4dd
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-04-02 13:53:35 -0500

    card post updated

--- components/CardPost.js
+++ components/CardPost.js
@@ -5,7 +5,9 @@ const CardPost = ({ post }) => {
     <li className="bg-base-100 rounded-3xl p-6 flex justify-between items-center">
       <div>
         <div className="font-bold mb-1">{post.title}</div>
-        <div className="opacity-80 leading-relaxed">{post.description}</div>
+        <div className="opacity-80 leading-relaxed max-h-32 overflow-scroll">
+          {post.description}
+        </div>
       </div>
       <button className="btn btn-square">⏫</button>
     </li>
