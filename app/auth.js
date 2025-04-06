4dece189658158562e98fbcc3f250c329866d886
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-04-05 22:54:14 -0500

    test push

--- app/auth.js
+++ app/auth.js
@@ -5,6 +5,14 @@ import Google from "next-auth/providers/google";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "@/libs/mongo";
 
+console.log("process.env.AUTH_SECRET", process.env.AUTH_SECRET);
+console.log("process.env.GOOGLE_CLIENT_ID", process.env.GOOGLE_CLIENT_ID);
+console.log(
+  "process.env.GOOGLE_CLIENT_SECRET",
+  process.env.GOOGLE_CLIENT_SECRET
+);
+console.log("process.env.RESEND_KEY", process.env.RESEND_KEY);
+
 const config = {
   providers: [
     Resend({
