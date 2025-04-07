b88ab6e413b1265eb07a74b77dcd4014afb0749a
Juan Gustavo Quinones <98677463+Swus1978@users.noreply.github.com>
2025-04-07 13:19:51 -0500

    packages updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config, { isServer }) => {
+    // Exclude 'snappy' from the client-side bundle
     if (!isServer) {
       config.externals.push("snappy");
     }
     return config;
   },
   eslint: {
+    // Skip ESLint during build for performance
     ignoreDuringBuilds: true,
   },
   experimental: {
+    // Force SWC transforms and limit the CPU cores used during build
     forceSwcTransforms: true,
-    cpus: 1, // Limit build process to 1 CPU core
+    cpus: 1,
   },
 };
 
-// For ESM (.mjs files)
+// Use ESM export syntax
 export default nextConfig;
